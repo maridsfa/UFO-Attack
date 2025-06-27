7e2f97c858537548a923d4738c59cb40677c9bc6
Mariana Farias <fariasdossantosmariana@gmail.com>
2025-06-27 09:26:54 -0300

    UFO Attack

---
+++ Form1.Designer.cs
@@ -0,0 +1,147 @@
+﻿namespace trab_prog
+{
+    partial class pb_Gameover
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(pb_Gameover));
+            this.pbNave = new System.Windows.Forms.PictureBox();
+            this.btStart = new System.Windows.Forms.Button();
+            this.pbLogo = new System.Windows.Forms.PictureBox();
+            this.bt_jogarnovamente = new System.Windows.Forms.Button();
+            this.pb_gv = new System.Windows.Forms.PictureBox();
+            this.pbGanhou = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pbNave)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbLogo)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pb_gv)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbGanhou)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pbNave
+            // 
+            this.pbNave.Image = ((System.Drawing.Image)(resources.GetObject("pbNave.Image")));
+            this.pbNave.Location = new System.Drawing.Point(421, 458);
+            this.pbNave.Margin = new System.Windows.Forms.Padding(4);
+            this.pbNave.Name = "pbNave";
+            this.pbNave.Size = new System.Drawing.Size(80, 100);
+            this.pbNave.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pbNave.TabIndex = 0;
+            this.pbNave.TabStop = false;
+            // 
+            // btStart
+            // 
+            this.btStart.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("btStart.BackgroundImage")));
+            this.btStart.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.btStart.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.btStart.Location = new System.Drawing.Point(347, 335);
+            this.btStart.Margin = new System.Windows.Forms.Padding(4);
+            this.btStart.Name = "btStart";
+            this.btStart.Size = new System.Drawing.Size(222, 52);
+            this.btStart.TabIndex = 1;
+            this.btStart.UseVisualStyleBackColor = true;
+            this.btStart.Click += new System.EventHandler(this.btStart_Click);
+            // 
+            // pbLogo
+            // 
+            this.pbLogo.Image = ((System.Drawing.Image)(resources.GetObject("pbLogo.Image")));
+            this.pbLogo.Location = new System.Drawing.Point(163, 66);
+            this.pbLogo.Name = "pbLogo";
+            this.pbLogo.Size = new System.Drawing.Size(598, 246);
+            this.pbLogo.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pbLogo.TabIndex = 3;
+            this.pbLogo.TabStop = false;
+            // 
+            // bt_jogarnovamente
+            // 
+            this.bt_jogarnovamente.Location = new System.Drawing.Point(347, 335);
+            this.bt_jogarnovamente.Name = "bt_jogarnovamente";
+            this.bt_jogarnovamente.Size = new System.Drawing.Size(222, 52);
+            this.bt_jogarnovamente.TabIndex = 4;
+            this.bt_jogarnovamente.Text = "Jogar novamente";
+            this.bt_jogarnovamente.UseVisualStyleBackColor = true;
+            this.bt_jogarnovamente.Visible = false;
+            this.bt_jogarnovamente.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // pb_gv
+            // 
+            this.pb_gv.Image = ((System.Drawing.Image)(resources.GetObject("pb_gv.Image")));
+            this.pb_gv.Location = new System.Drawing.Point(163, 66);
+            this.pb_gv.Name = "pb_gv";
+            this.pb_gv.Size = new System.Drawing.Size(605, 246);
+            this.pb_gv.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pb_gv.TabIndex = 5;
+            this.pb_gv.TabStop = false;
+            this.pb_gv.Visible = false;
+            // 
+            // pbGanhou
+            // 
+            this.pbGanhou.Image = ((System.Drawing.Image)(resources.GetObject("pbGanhou.Image")));
+            this.pbGanhou.Location = new System.Drawing.Point(163, 66);
+            this.pbGanhou.Name = "pbGanhou";
+            this.pbGanhou.Size = new System.Drawing.Size(605, 246);
+            this.pbGanhou.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pbGanhou.TabIndex = 6;
+            this.pbGanhou.TabStop = false;
+            this.pbGanhou.Visible = false;
+            // 
+            // pb_Gameover
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.ClientSize = new System.Drawing.Size(912, 583);
+            this.Controls.Add(this.pbGanhou);
+            this.Controls.Add(this.pb_gv);
+            this.Controls.Add(this.bt_jogarnovamente);
+            this.Controls.Add(this.pbLogo);
+            this.Controls.Add(this.btStart);
+            this.Controls.Add(this.pbNave);
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.MaximizeBox = false;
+            this.Name = "pb_Gameover";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "SPACE INVADERS";
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            ((System.ComponentModel.ISupportInitialize)(this.pbNave)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbLogo)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pb_gv)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbGanhou)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pbNave;
+        private System.Windows.Forms.Button btStart;
+        private System.Windows.Forms.PictureBox pbLogo;
+        private System.Windows.Forms.Button bt_jogarnovamente;
+        private System.Windows.Forms.PictureBox pb_gv;
+        private System.Windows.Forms.PictureBox pbGanhou;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,305 @@
+﻿using System;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Windows.Forms;
+
+namespace trab_prog
+{
+
+    //SPACE INVADERS
+    //MARIA ISABEL SOARES E MARIANA FARIAS - TRÔ 7M
+
+
+    public partial class pb_Gameover : Form
+    {
+        private int _alienslinhaAtaqueAuto = 4;
+        private int _maxAlienslinhaAtaqueAuto = 6;
+        private int _maxInitialAliens = 3;
+        
+        public pb_Gameover()
+        {
+            InitializeComponent();
+            pbNave.Visible = false;
+            pbLogo.Visible = true;
+
+        }
+
+        #region Eventos do Formulario
+        private void btStart_Click(object sender, EventArgs e)
+        {
+            pbNave.Visible = true;
+            btStart.Visible = false;
+            btStart.Enabled = false;
+            pbLogo.Visible = false;
+            bt_jogarnovamente.Visible = false;
+            pb_gv.Visible = false;
+            
+            //Gera os aliens de forma dinâmica
+            System.Windows.Forms.Timer geraLinhasAtaqueAlien = new System.Windows.Forms.Timer();
+            geraLinhasAtaqueAlien.Interval = 5000;
+            geraLinhasAtaqueAlien.Tick += GeraAlien_Tick;
+            geraLinhasAtaqueAlien.Start();
+
+            //Coloca os primeiros 3 aliens em colunas no formulário para o ataque
+            int alienX = 50, alienY = 0;
+
+            double incrementalNextPosition = this.Size.Width / _maxInitialAliens;
+
+            for (int m = 1; m <= _maxInitialAliens; m++)
+            {
+                PictureBox pbAlien = new PictureBox();
+                pbAlien.Name = "Alien";
+                pbAlien.SizeMode = PictureBoxSizeMode.Zoom;
+                pbAlien.Image = Image.FromFile("../../Figuras/aliens1.png");
+                pbAlien.Size = new System.Drawing.Size(50, 50);
+                pbAlien.Visible = true;
+                pbAlien.Location = new Point(alienX, alienY);
+                this.Controls.Add(pbAlien);
+
+
+                alienX += (int)Math.Ceiling(incrementalNextPosition);
+
+            }
+
+            //Cria um timer e o evento Tick do timer para os aliens descerem no formulário
+            System.Windows.Forms.Timer tmMoveAliens = new System.Windows.Forms.Timer();
+            tmMoveAliens.Interval = 1000;
+
+            //Representa o método que vai ser executado para tratar o evento Tick (quando o tempo do timer acabar) - irá gerar o resto dos aliens em colunas
+            tmMoveAliens.Tick += TmMoveAliens_Tick; 
+            tmMoveAliens.Start();
+            
+        }
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            //Desabilita o som ao pressionar as teclas
+            e.SuppressKeyPress = true;
+            bt_jogarnovamente.Visible = false;
+            int passo = 5;
+
+
+            if (e.KeyCode == Keys.D)
+            {
+                if (pbNave.Location.X + pbNave.Width + passo <= 700)
+                {
+                    pbNave.Location = new Point(pbNave.Location.X + passo, pbNave.Location.Y);
+                }
+            }
+            if (e.KeyCode == Keys.A)
+            {
+                if (pbNave.Location.X + pbNave.Left + passo >= -39)
+                {
+                    pbNave.Location = new Point(pbNave.Location.X - passo, pbNave.Location.Y);
+                }
+            }
+
+
+            if (e.KeyCode == Keys.Space)
+            {
+                int naveMidllePosition = pbNave.Size.Width / 2;
+
+                //Cria de forma dinâmica os picture box dos tiros quando a tecla espaço for pressionada
+                PictureBox pbTiro = new PictureBox();
+                pbTiro.Name = Guid.NewGuid().ToString();
+                pbTiro.SizeMode = PictureBoxSizeMode.Zoom;
+                pbTiro.Image = Image.FromFile("../../Figuras/laser.png");
+                pbTiro.Size = new System.Drawing.Size(10, 30);
+                pbTiro.Visible = true;
+                pbTiro.Location = new Point(pbNave.Location.X + naveMidllePosition, pbNave.Location.Y - 47);
+                this.Controls.Add(pbTiro);
+
+                //Cria de forma dinâmica o timer e o evento Tick dos tiros 
+                System.Windows.Forms.Timer tmTiro = new System.Windows.Forms.Timer();
+                tmTiro.Interval = 30;
+                tmTiro.Tag = pbTiro.Name;
+
+                //Representa o método que vai ser executado para tratar o evento Tick (quando o tempo do timer acabar)
+                tmTiro.Tick += TmTiro_Tick; 
+                tmTiro.Start();
+
+            }
+
+        } 
+
+        #endregion
+
+        #region Métodos para Identificar condição do Alien
+
+        //Identifica se o tiro atingiu algum alien
+        private bool alienAtingido(PictureBox pbAlien, PictureBox pbTiro)
+        {
+            bt_jogarnovamente.Visible = false;
+
+            if ((pbTiro.Location.X >= pbAlien.Location.X) && pbTiro.Location.X <= (pbAlien.Location.X + 50))
+            {
+
+                if (pbTiro.Location.Y <= (pbAlien.Location.Y + 50) && pbTiro.Location.Y >= pbAlien.Location.Y)
+                {
+                    return true;
+                }
+            }
+
+            return false;
+        }
+
+
+        private bool validaGameOver(PictureBox pbAlien)
+        {
+            //Identifica se o alien aterrisou
+            if (pbAlien.Location.Y >= 430)
+            {
+                return true;
+            }
+
+            //Idetifica se o alien atingiu a nave
+            int naveRightSideLocation = pbNave.Location.X + 80;
+            int alienRightSideLocation = pbAlien.Location.X + 50;
+
+            if ((pbAlien.Location.X >= pbNave.Location.X) && (pbAlien.Location.X <= naveRightSideLocation)
+                    || (alienRightSideLocation >= pbNave.Location.X) && (alienRightSideLocation <= naveRightSideLocation))
+            {
+                if (pbAlien.Location.Y + 50 >= pbNave.Location.Y)
+                {
+                    return true;
+                }
+            }
+            
+            return false;
+        }
+
+
+        #endregion
+
+        #region Métodos Tick dos Timers
+
+        private void GeraAlien_Tick(object sender, EventArgs e)
+        {
+            int alienXPosition = 50;
+
+            for (int m = 1; m <= _alienslinhaAtaqueAuto; m++)
+            {
+                PictureBox pbAlien = new PictureBox();
+                pbAlien.Name = "Alien";
+                pbAlien.SizeMode = PictureBoxSizeMode.Zoom;
+                pbAlien.Image = Image.FromFile("../../Figuras/aliens1.png");
+                pbAlien.Size = new System.Drawing.Size(50, 50);
+                pbAlien.Visible = true;
+                pbAlien.Location = new Point(alienXPosition, 0);
+                this.Controls.Add(pbAlien);
+
+                //Divide a janela pela quantidade de aliens na linha
+                double incrementalNextPosition = this.Size.Width / _alienslinhaAtaqueAuto; 
+                //Converte esse resultado para para próximo número inteiro
+                alienXPosition += (int)Math.Ceiling(incrementalNextPosition); 
+
+            }
+
+            _alienslinhaAtaqueAuto++;
+
+            //Identifica se o máximo numero de aliens por linha de ataque foi gerado
+            if (_alienslinhaAtaqueAuto > _maxAlienslinhaAtaqueAuto)
+            {
+                System.Windows.Forms.Timer timer = (System.Windows.Forms.Timer)sender;
+                timer.Stop();
+                timer.Dispose();
+            }
+        }
+
+        private void TmTiro_Tick(object sender, EventArgs e)
+        {
+            System.Windows.Forms.Timer timer = (System.Windows.Forms.Timer)sender;
+
+            //Informa para o timer a picture box que ele controla
+            PictureBox pbTiro = this.Controls.OfType<PictureBox>().FirstOrDefault(c => c.Name == timer.Tag.ToString());
+            
+            //Pergunta ao formUlário se o tiro ainda está na área visivel do jogo
+            bool tiroAtivo = pbTiro.Location.Y > 0;
+
+            if (tiroAtivo)
+            {
+                //O uso do foreach, em suma, pergunta ao formulario quais são os aliens ativos na tela e "percorre" eles
+                foreach (PictureBox alien in this.Controls.OfType<PictureBox>().Where(c => c.Name == "Alien"))
+                {
+                    //Verifica se algum dos diversos aliens foi atingido
+                    if (alienAtingido(alien, pbTiro))
+                    {  
+                        //Se sim, o alien é destruído
+                        alien.Visible = false;
+                        this.Controls.Remove(alien);
+                        alien.Dispose();
+                      
+                        tiroAtivo = false;
+                        break;
+
+                    }
+                    
+                }
+
+                pbTiro.Location = new Point(pbTiro.Location.X, pbTiro.Location.Y - 5);
+            }
+
+            //Se o tiro não está na area do jogo ou atingiu algum alien, ele é destruido
+            if (!tiroAtivo)
+            {
+                timer.Stop();
+                timer.Dispose();
+                pbTiro.Visible = false;
+                this.Controls.Remove(pbTiro); 
+                pbTiro.Dispose();
+
+                //Valida se ainda ainda existem aliens no formulário
+                bool todosAliensEliminados = this.Controls.OfType<PictureBox>().Count(c => c.Name == "Alien") == 0;
+
+                if (todosAliensEliminados)
+                {
+                    pbGanhou.Visible = true;
+                    bt_jogarnovamente.Visible = true;
+                }
+            }
+
+        }
+
+        private void TmMoveAliens_Tick(object sender, EventArgs e)
+        {
+            bool gameOver = false;
+
+            //Pergunta ao formulário uma lista de todos os Picture Box que são do tipo "Alien" e percorre essa lista com o foreach
+            foreach (PictureBox alien in this.Controls.OfType<PictureBox>().Where(c => c.Name == "Alien"))
+            {
+                gameOver = validaGameOver(alien);
+
+                if (gameOver)
+                {
+                    //Type cast (identifica o timer chamador do evento)
+                    System.Windows.Forms.Timer timer = (System.Windows.Forms.Timer)sender;
+
+                    timer.Stop(); //Desativa o timer que gera o movimento dos aliens
+                    timer.Dispose();
+
+                    break;
+                }
+
+                //Não sendo GameOver o timer seguirá movimentando os aliens
+                alien.Location = new Point(alien.Location.X, alien.Location.Y + 10);
+            }
+
+            if (gameOver)
+            {
+                bt_jogarnovamente.Visible = true;
+                pb_gv.Visible = true;
+            } 
+
+        }
+
+
+        #endregion
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Application.Restart();
+            
+        }
+    }
+}
